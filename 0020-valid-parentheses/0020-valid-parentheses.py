e9c38acc19c3935f74bbb8df16e8646f5b7a8036
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-13 14:20:49 +0530

    Time: 34 ms (67.35%), Space: 16.5 MB (92.75%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack =[]
+        ctoo = {')':'(',']':'[',"}":'{'}
+        for i in s:
+            if i in ctoo:
+                if stack and stack[-1]== ctoo[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
