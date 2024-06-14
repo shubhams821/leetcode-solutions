b9599950ccaf989500e77591c42024b9a1b89fb4
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-14 11:43:59 +0530

    Time: 114 ms (96.63%), Space: 18.1 MB (66.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -6,6 +6,6 @@ class Solution:
         l =0
         for r in range(len(nums)):
             if nums[r]:
-                nums[l], nums[r] = nums[r],nums[l]
+                nums[l],nums[r] = nums[r],nums[l]
                 l+=1
         return nums
