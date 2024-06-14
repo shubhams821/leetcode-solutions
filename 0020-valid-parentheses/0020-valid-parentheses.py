c35b7e80f439bc8a520bb86b4c647ea90487b6ef
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-14 17:50:36 +0530

    Time: 37 ms (46.32%), Space: 16.6 MB (60.41%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -11,3 +11,14 @@ class Solution:
             else:
                 stack.append(i)
         return True if not stack else False
+        stack =[]
+        ctoo = {')':'(',']':'[','}':'{'}
+        for i in s:
+            if i in ctoo:
+                if stack and stack[-1]==ctoo[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+        return True if not stack else False
