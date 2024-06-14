5c46ffc97fd1f6f945586e7fc86a8e038cca3e17
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-14 17:47:58 +0530

    Time: 4 ms (37.84%), Space: 15.4 MB (7.39%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void recur(vector<int> nums, vector<int> temp, int idx){
+        if (idx == nums.size()){
+            ans.push_back(temp);
+            return;
+        }
+        recur(nums, temp,idx+1);
+        temp.push_back(nums[idx]);
+        recur(nums, temp,idx+1);
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> temp;
+        int i = 0;
+        ans.clear();
+        recur(nums,temp, i);
+        return ans;
+    }
+};
