6c3fe52fd257690b3fdf8368ae5efea2c0a83ea7
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-15 20:59:11 +0530

    Time: 41 ms (19.13%), Space: 16.5 MB (59.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,8 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        stack =[]
-        ctoo = {')':'(',']':'[',"}":'{'}
+        stack = []
+        ctoo = {']':'[',')':'(','}':'{'}
+
         for i in s:
+            # print(stack)
             if i in ctoo:
                 if stack and stack[-1]== ctoo[i]:
                     stack.pop()
@@ -10,15 +12,5 @@ class Solution:
                     return False
             else:
                 stack.append(i)
-        return True if not stack else False
-        stack =[]
-        ctoo = {')':'(',']':'[','}':'{'}
-        for i in s:
-            if i in ctoo:
-                if stack and stack[-1]==ctoo[i]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(i)
+        # print(stack)
         return True if not stack else False
