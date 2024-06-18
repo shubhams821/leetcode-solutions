c1190ce42d91e706e5864538e946522882b28804
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-18 17:13:46 +0530

    Time: 49 ms (31.5%), Space: 17 MB (63.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -3,22 +3,20 @@ class Solution:
         Rows, Cols = len(matrix), len(matrix[0])
 
         top, bot = 0, Rows-1
-        while(top<= bot):
-            print(top, bot)
-            row = (top + bot)//2
 
+        while (top<=bot):
+            row = (top+bot)//2
             if matrix[row][0] > target:
                 bot = row-1
-            elif matrix[row][-1]< target:
-                top = row+1
+            elif matrix[row][-1] < target:
+                top = row +1
             else:
                 break
-        row = (top + bot)//2
-        l,r = 0, Cols-1
+        row = (top+bot)//2
+        l, r = 0, Cols-1
 
         while(l<=r):
-            m = (l+r)//2
-
+            m=  (l+r)//2
             if matrix[row][m] < target:
                 l = m+1
             elif matrix[row][m] > target:
