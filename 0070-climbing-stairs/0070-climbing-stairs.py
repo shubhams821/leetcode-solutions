1bac84e8cd8ad316d6f3893545683345a2623416
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-06-20 20:14:49 +0530

    Time: 35 ms (51.92%), Space: 16.4 MB (66.53%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         first, second = 1,1
         for i in range(n-1):
-            temp = first+second
+            temp = first + second
             second = first
             first = temp
         return first
