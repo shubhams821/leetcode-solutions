4877ee59931289e1ad9be38125191643d4ef2d8e
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-07-02 08:25:42 +0530

    Time: 57 ms (37.95%), Space: 17.8 MB (26.05%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.py
@@ -12,7 +12,6 @@ class Solution:
             m = (l+r)//2
             root = TreeNode(nums[m])
             root.left = helper(l,m-1)
-            root.right = helper(m+1,r)
+            root.right = helper(m+1, r)
             return root
         return helper(0, len(nums)-1)
-        
