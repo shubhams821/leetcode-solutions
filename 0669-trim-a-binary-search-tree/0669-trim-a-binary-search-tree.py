1fa8f77b535166ddf813060ef12170f6f5a353f3
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-07-23 09:07:09 +0530

    Time: 47 ms (41.26%), Space: 19.5 MB (49.41%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -8,9 +8,9 @@ class Solution:
     def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
         if not root:
             return None
-        if root.val > high:
+        if root.val> high:
             return self.trimBST(root.left, low, high)
-        if root.val< low:
+        if root.val<low:
             return self.trimBST(root.right, low, high)
         root.left = self.trimBST(root.left, low, high)
         root.right = self.trimBST(root.right, low, high)
