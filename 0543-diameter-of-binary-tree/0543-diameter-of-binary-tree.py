86f524f8eef2b34074986336cedfd0d3ba7a933c
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-07-26 09:34:38 +0530

    Time: 37 ms (96.78%), Space: 19.3 MB (26.27%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -7,7 +7,6 @@
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = [0]
-
         def dfs(node):
             if not node:
                 return -1
