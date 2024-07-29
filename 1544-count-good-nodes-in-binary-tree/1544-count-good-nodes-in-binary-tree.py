7557824d4a7d02d1dbbe64d46101bfd96178ea48
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-07-29 10:12:10 +0530

    Time: 137 ms (42.86%), Space: 31.1 MB (67.87%) - LeetHub

--- 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/1544-count-good-nodes-in-binary-tree.py
@@ -9,9 +9,9 @@ class Solution:
         def dfs(node, maxVal):
             if not node:
                 return 0
-            res = 1 if node.val>= maxVal else 0
+            res = 1 if node.val >= maxVal else 0
             maxVal = max(maxVal, node.val)
-            res+= dfs(node.right,maxVal)
-            res+= dfs(node.left, maxVal)
+            res +=dfs(node.left, maxVal)
+            res += dfs(node.right, maxVal)
             return res
         return dfs(root, root.val)
