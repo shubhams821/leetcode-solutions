67755f11d6b8707a63eec382d90d1f700144ed1e
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-07-31 10:06:42 +0530

    Time: 45 ms (64.38%), Space: 18.3 MB (69.98%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.py
@@ -7,9 +7,17 @@
 class Solution:
     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         q = deque([root])
-        
+        rightSide = None
+        res = []
         while q:
-            node = q.popleft()
-            if node.right: q.append(node.right)
-            if node.left: q.append(node.left)
-        return node.val
+            qLen= len(q)
+            rightSide = None
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.right)
+                    q.append(node.left)
+            if rightSide:
+                res.append(rightSide.val)
+        return res[-1]
