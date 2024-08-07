dd9400b731ec0696f1a42ee214eee599c9e1d010
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-07 09:28:37 +0530

    Time: 41 ms (60.12%), Space: 18.4 MB (43.57%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -9,8 +9,7 @@ class Solution:
         def valid(node, low, high):
             if not node:
                 return True
-            if node.val <= low or node.val >= high:
+            if node.val <=low or node.val>= high:
                 return False
-            else:
-                return valid(node.left, low, node.val) and valid(node.right, node.val, high)
+            return valid(node.left, low, node.val) and valid(node.right, node.val, high)
         return valid(root, float("-inf"), float("inf"))
