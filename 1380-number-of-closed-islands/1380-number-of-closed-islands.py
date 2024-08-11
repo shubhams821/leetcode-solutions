7a29ceb1db9bc7f235514beb384842a03d821aec
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-11 22:05:37 +0530

    Time: 115 ms (21.93%), Space: 19 MB (6.24%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -3,19 +3,18 @@ class Solution:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
         def dfs(i,j):
-            if i <0 or j < 0 or i == Rows or j == Cols:
+            if i<0 or j<0 or i == Rows or j== Cols:
                 return 0
-            if grid[i][j] == 1 or (i,j) in visit:
+            if grid[i][j] ==1 or (i,j) in visit:
                 return 1
-            
             visit.add((i,j))
-            return min( dfs(i,j+1),
+            return min(dfs(i,j+1), 
                         dfs(i+1, j),
                         dfs(i,j-1),
-                        dfs(i-1,j))
-        res = 0
+                        dfs(i-1, j))
+        res =0
         for i in range(Rows):
             for j in range(Cols):
                 if not grid[i][j] and (i,j) not in visit:
-                    res += dfs(i,j)
+                    res+= dfs(i,j)
         return res
