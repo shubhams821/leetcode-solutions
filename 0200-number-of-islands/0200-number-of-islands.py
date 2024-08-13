b162b6073604a861877856756dad26b9b57d439f
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-13 09:30:47 +0530

    Time: 314 ms (12.74%), Space: 24.3 MB (21.88%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -9,23 +9,22 @@ class Solution:
             q = collections.deque()
             visit.add((i,j))
             q.append((i,j))
-
+            
             while q:
                 row, col = q.popleft()
                 directions = [[1,0],[0,1],[-1,0],[0,-1]]
                 for dr, dc in directions:
-                    r, c = row +dr, col+dc
-                    if r in range(rows) and \
-                        c in range(cols) and \
-                        grid[r][c] == "1" and \
-                        (r,c) not in visit:
+                    r,c = row+dr, col+dc
+                    if (r in range(rows) and
+                        c in range(cols) and
+                        grid[r][c] == "1" and
+                        (r,c) not in visit):
                         q.append((r,c))
                         visit.add((r,c))
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] =="1" and \
-                   (r,c) not in visit:
-                   bfs(r,c)
-                   island+=1
-        return island                
-
+                if grid[r][c] =="1" and\
+                    (r,c) not in visit:
+                    bfs(r,c)
+                    island+=1
+        return island
