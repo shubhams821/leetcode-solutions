fe4df65b9d6e7a3dcf2eee73dfdafe0c00409f6c
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-14 23:52:51 +0530

    Time: 123 ms (21.49%), Space: 18.4 MB (12.08%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -5,17 +5,18 @@ class Solution:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
         def dfs(i,j):
-            if i not in range(Rows) or j not in range(Cols) or not grid[i][j] or (i,j) in visit:
+            if i not in range(Rows) or j not in range(Cols) or not grid[i][j]:
+                return 0
+            if (i,j) in visit:
                 return 0
             visit.add((i,j))
-            return (1 + dfs(i,j+1)+
-                        dfs(i+1,j)+
-                        dfs(i,j-1)+
-                        dfs(i-1,j))
-        res =0
+            return (1 + dfs(i,j+1)
+                    + dfs(i+1,j)
+                    + dfs(i,j-1)      
+                    + dfs(i-1,j))
+        res = 0
         for r in range(Rows):
             for c in range(Cols):
                 if grid[r][c] and (r,c) not in visit:
-                    temp = dfs(r,c)
-                    res = max(temp, res)
+                    res = max(res, dfs(r,c))
         return res
