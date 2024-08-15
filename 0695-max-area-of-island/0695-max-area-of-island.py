5b770cea0d6e30263922b2e903d830f8d6181931
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-15 10:24:59 +0530

    Time: 130 ms (11.81%), Space: 18.5 MB (12.11%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,22 +1,23 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
+
         def dfs(i,j):
             if i not in range(Rows) or j not in range(Cols) or not grid[i][j]:
                 return 0
             if (i,j) in visit:
                 return 0
             visit.add((i,j))
-            return (1 + dfs(i,j+1)
-                    + dfs(i+1,j)
-                    + dfs(i,j-1)      
-                    + dfs(i-1,j))
+            return (1 + dfs(i,j+1)+
+                        dfs(i+1,j)+
+                        dfs(i,j-1)+
+                        dfs(i-1,j))
         res = 0
         for r in range(Rows):
             for c in range(Cols):
-                if grid[r][c] and (r,c) not in visit:
-                    res = max(res, dfs(r,c))
+                if grid[r][c] and(r,c) not in visit:
+                    tmp = dfs(r,c)
+                    res = max(res,tmp)
+
         return res
