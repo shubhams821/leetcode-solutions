a5cb370aef975f0b509ac1324ea1e9aeabfd3aca
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-20 09:55:32 +0530

    Time: 228 ms (27.03%), Space: 18.2 MB (57.02%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -4,10 +4,10 @@ class Solution:
         pac, atl = set(), set()
 
         def dfs(r,c, visit, prevHeight):
-            if ((r,c) in visit or
+            if ((r,c) in visit or 
                 r not in range(Rows) or
                 c not in range(Cols) or
-                heights[r][c]< prevHeight):
+                heights[r][c] < prevHeight):
                 return
             visit.add((r,c))
             dfs(r+1,c, visit, heights[r][c])
@@ -16,11 +16,11 @@ class Solution:
             dfs(r,c-1, visit, heights[r][c])
         for c in range(Cols):
             dfs(0,c, pac, heights[0][c])
-            dfs(Rows-1, c, atl, heights[Rows-1][c])
+            dfs(Rows-1,c , atl, heights[Rows-1][c])
         for r in range(Rows):
-            dfs(r,0,pac, heights[r][0])
+            dfs(r,0, pac, heights[r][0])
             dfs(r, Cols-1, atl, heights[r][Cols-1])
-        res =[]
+        res = []
         for r in range(Rows):
             for c in range(Cols):
                 if (r,c) in pac and (r,c) in atl:
