31f5c6b9ac85b25b011f55ef7331ccf17ae89ff1
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-21 10:07:50 +0530

    Time: 122 ms (47.97%), Space: 17.8 MB (77.92%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,32 +1,23 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         Rows, Cols = len(board), len(board[0])
+        visit = set()
         def dfs(i,j):
-            print(i,j)
-            if (i <0 or i == Rows or j <0 or j == Cols or board[i][j] != "O"):
-                return 
-            board[r][c] = "T"
+            if (i not in range(Rows) or j not in range(Cols) or board[i][j] != "O"):
+                return
+            board[i][j] = "T"
+            dfs(i,j+1)
             dfs(i+1,j)
+            dfs(i,j-1)
             dfs(i-1,j)
             return
-            dfs(i,j+1)
-            dfs(i,j-1)
         for r in range(Rows):
             for c in range(Cols):
-                if (board[r][c] == "O" and 
-                    (r in [0, Rows-1] or c in [0,Cols-1])):
-                    print(r,c)
-                    # break
+                if board[r][c] == "O" and (r in [0, Rows-1] or c in [0, Cols-1]):
                     dfs(r,c)
-                    
-                
-
         for r in range(Rows):
             for c in range(Cols):
                 if board[r][c] == "O":
-                    board[r][c] = "X"
-
-        for r in range(Rows):
-            for c in range(Cols):
-                if board[r][c] == "T":
+                    board[r][c] ="X"
+                elif board[r][c] == "T":
                     board[r][c] = "O"
