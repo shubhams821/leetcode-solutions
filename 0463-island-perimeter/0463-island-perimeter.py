3b312b04ba2102799362d5d4063a06b989be5f6e
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-22 11:21:36 +0530

    Time: 525 ms (7.83%), Space: 24 MB (7.06%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,16 +2,17 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
+
         def dfs(i,j):
             if i not in range(Rows) or j not in range(Cols) or not grid[i][j]:
                 return 1
             if (i,j) in visit:
                 return 0
             visit.add((i,j))
-            perim = dfs(i,j+1)
-            perim += dfs(i+1,j)
+            perim = dfs(i+1,j)
+            perim+= dfs(i,j+1)
+            perim+= dfs(i-1,j)
             perim += dfs(i,j-1)
-            perim += dfs(i-1,j)
             return perim
         for r in range(Rows):
             for c in range(Cols):
