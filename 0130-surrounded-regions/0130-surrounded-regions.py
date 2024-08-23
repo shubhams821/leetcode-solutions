e600540b059ea7e0a21c7a36b3058d23e463e0ad
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-23 09:30:57 +0530

    Time: 117 ms (72.5%), Space: 18 MB (36%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,8 +1,9 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         Rows, Cols = len(board), len(board[0])
+        
         def dfs(i,j):
-            if i not in range(Rows) or j not in range(Cols) or board[i][j] != "O":
+            if i not in range(Rows) or j not in range(Cols) or board[i][j]!= "O":
                 return
             board[i][j] = "T"
             dfs(i,j+1)
@@ -12,12 +13,12 @@ class Solution:
             return
         for r in range(Rows):
             for c in range(Cols):
-                if r in [0, Rows-1] or c in [0, Cols-1]:
+                if r in [0,Rows-1] or c in [0, Cols-1]:
                     dfs(r,c)
-        
         for r in range(Rows):
             for c in range(Cols):
                 if board[r][c] == "O":
                     board[r][c] = "X"
-                elif board[r][c] =="T":
-                    board[r][c] = "O" 
+                elif board[r][c] == "T":
+                    board[r][c] ="O"
+                
