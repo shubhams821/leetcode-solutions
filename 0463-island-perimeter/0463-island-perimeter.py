7216bf71af5b1deecd88790384fa449d51306896
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-25 19:04:22 +0530

    Time: 560 ms (5.04%), Space: 24 MB (7.09%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -4,15 +4,15 @@ class Solution:
         visit = set()
 
         def dfs(i,j):
-            if i not in range(Rows) or j not in range(Cols) or not grid[i][j]:
+            if i not in range(Rows) or j not in range(Cols) or  not grid[i][j]:
                 return 1
             if (i,j) in visit:
                 return 0
             visit.add((i,j))
-            perim = dfs(i+1,j)
-            perim+= dfs(i,j+1)
-            perim+= dfs(i-1,j)
+            perim = dfs(i,j+1)
+            perim += dfs(i+1,j)
             perim += dfs(i,j-1)
+            perim += dfs(i-1,j)
             return perim
         for r in range(Rows):
             for c in range(Cols):
