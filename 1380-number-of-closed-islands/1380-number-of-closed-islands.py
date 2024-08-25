62115baa6a6089a0bb16fb4fc7df728041028048
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-25 22:15:08 +0530

    Time: 124 ms (10.21%), Space: 18.8 MB (10.45%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -2,16 +2,20 @@ class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
+
         def dfs(i,j):
             if i not in range(Rows) or j not in range(Cols):
                 return 0
             if (i,j) in visit or grid[i][j]:
                 return 1
             visit.add((i,j))
-            return min(dfs(i,j+1),dfs(i+1,j), dfs(i,j-1), dfs(i-1,j))
+            return min(dfs(i,j+1),
+                        dfs(i+1,j),
+                        dfs(i,j-1),
+                        dfs(i-1,j))
         res = 0
         for r in range(Rows):
             for c in range(Cols):
-                if not grid[r][c]  and (r,c) not in visit:
-                    res += dfs(r,c)
+                if not grid[r][c] and (r,c) not in visit:
+                    res+= dfs(r,c)
         return res
