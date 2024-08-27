5b643ff25b3d0b6719128ca77a2135367fbd43a4
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-27 09:38:09 +0530

    Time: 88 ms (66.57%), Space: 18.3 MB (51.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -3,18 +3,16 @@ class Solution:
         preMap = {i:[] for i in range(numCourses)}
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
-        #VisitSet = All the courses along the curr DFS Path
+        
         visit = set()
         def dfs(crs):
-            if crs in visit:
-                return False
-            if preMap[crs] ==[]:
-                return True
+            if crs in visit: return False
+            if preMap[crs] ==[]: return True
             visit.add(crs)
             for pre in preMap[crs]:
                 if not dfs(pre): return False
             visit.remove(crs)
-            preMap[crs] = []
+            preMap[crs] =[]
             return True
         for crs in range(numCourses):
             if not dfs(crs): return False
