1e4504e14df1c85b0c7fa3214da8e8afa2f24196
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-08-28 09:26:37 +0530

    Time: 352 ms (5.16%), Space: 24.5 MB (17.69%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -12,13 +12,15 @@ class Solution:
                     row, col = q.popleft()
                     directions = [[1,0],[0,1],[-1,0],[0,-1]]
                     for dr, dc in directions:
-                        r,c = row+dr, col+dc
-                        if (r,c) not in visit and r in range(Rows) and c in range(Cols) and grid[r][c] == "1":
+                        r, c= row+dr, col+dc
+                        if r in range(Rows) and c in range(Cols) and (r,c) not in visit and grid[r][c] =="1":
                             bfs(r,c)
         island = 0
         for r in range(Rows):
             for c in range(Cols):
-                if grid[r][c] == "1" and (r,c) not in visit:
+                if grid[r][c] =="1" and (r,c) not in visit:
                     bfs(r,c)
-                    island += 1
+                    island+=1
         return island
+        
+            
