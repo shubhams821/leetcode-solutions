3a2bc6764c7bd88ccf92512e04f998995966c21e
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-01 21:54:19 +0530

    Time: 149 ms (10.15%), Space: 22.9 MB (89.46%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,35 +1,11 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        i = 0
-        while target > matrix[i][i]:
-            if i+1 == len(matrix) or i+1 == len(matrix[0]):
-                break
-            i+=1
-        i,j = i,i
-        while i< len(matrix):
-            l,r =0, len(matrix[0])
-            while l<=r:
-                m = (l+r)//2
-                if m >= len(matrix[0]):
-                    break
-                if matrix[i][m] == target:
-                    return True
-                elif matrix[i][m] > target:
-                    r = m-1
-                elif matrix[i][m] < target:
-                    l = m+1
-            i+=1
-        while j< len(matrix[0]):
-            l, r =0, len(matrix)
-            while l<=r:
-                m = (l+r)//2
-                if m >= len(matrix):
-                    break
-                if matrix[m][j] == target:
-                    return True
-                elif matrix[m][j] > target:
-                    r = m-1
-                elif matrix[m][j] < target:
-                    l = m+1
-            j+=1
+        r, c = 0, len(matrix[0])-1
+        while r < len(matrix) and c  >=0:
+            if matrix[r][c] == target:
+                return True
+            if matrix[r][c] > target:
+                c-=1
+            else:
+                r+=1
         return False
