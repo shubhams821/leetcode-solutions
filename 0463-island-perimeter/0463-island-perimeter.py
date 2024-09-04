80913760d2b9652ced658911a0913b72d4bed45e
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-05 05:17:04 +0530

    Time: 700 ms (5.03%), Space: 17.9 MB (37.39%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -2,19 +2,24 @@ class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
-
-        def dfs(i,j):
-            if i not in range(Rows) or j not in range(Cols) or not grid[i][j]:
-                return 1
-            if (i,j) in visit:
-                return 0
+        perim =[0]
+        def bfs(i,j):
+            q = collections.deque()
+            q.append([i,j])
             visit.add((i,j))
-            perim = dfs(i,j+1)
-            perim+= dfs(i+1,j)
-            perim += dfs(i,j-1)
-            perim += dfs(i-1,j)
-            return perim
+            while q:
+                for i in range(len(q)):
+                    row, col = q.popleft()
+                    directions =[[1,0],[0,1],[-1,0],[0,-1]]
+                    for dr, dc in directions:
+                        r, c = row+dr, col +dc
+                        if r in range( Rows) and c in range(Cols) and (r,c) not in visit and grid[r][c]:
+                            q.append([r,c])
+                            visit.add((r,c))
+                        if r not in range(Rows) or c not in range(Cols) or not grid[r][c]:
+                            perim[0]+=1
         for r in range(Rows):
             for c in range(Cols):
-                if grid[r][c] and (r,c) not in visit:
-                    return dfs(r,c)
+                if (r,c) not in visit and grid[r][c]:
+                    bfs(r,c)
+        return perim[0]
