ffb5355889abdd0fd3df661c2eac3e1f4fa64b64
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-05 10:01:48 +0530

    Time: 383 ms (32.88%), Space: 18.6 MB (46.5%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -3,7 +3,7 @@ class Solution:
         edges = collections.defaultdict((list))
         for u,v,w in times:
             edges[u].append((v,w))
-
+        
         minHeap = [(0,k)]
         visit = set()
         t =0
@@ -12,8 +12,8 @@ class Solution:
             if n1 in visit:
                 continue
             visit.add(n1)
-            t = max(t, w1)
+            t = max(t,w1)
             for n2, w2 in edges[n1]:
                 if n2 not in visit:
-                    heapq.heappush(minHeap, (w1+w2, n2))
+                        heapq.heappush(minHeap, (w1+w2, n2))
         return t if len(visit) == n else -1
