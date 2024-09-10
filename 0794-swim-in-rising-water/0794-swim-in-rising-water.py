5d607e243c5f856e48adc7c3ed4b73608c24f00c
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-10 09:48:10 +0530

    Time: 86 ms (70.1%), Space: 17.4 MB (22.66%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.py
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.py
@@ -2,20 +2,15 @@ class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         Rows, Cols = len(grid), len(grid[0])
 
-        minHeap =[(grid[0][0],0,0)]
+        minHeap = [(grid[0][0],0,0)]
         directions = [[0,1],[0,-1],[1,0],[-1,0]]
         visit = set()
-        visit.add((0,0))
         while minHeap:
             tmp, r,c = heapq.heappop(minHeap)
-            if (r,c) ==(Rows-1, Cols-1): 
+            if (r,c) == (Rows-1, Cols-1):
                 return tmp
             for dr, dc in directions:
-                nr,nc = r+dr, c+dc
+                nr, nc = r+dr, c+dc
                 if nr in range(Rows) and nc in range(Cols) and (nr,nc) not in visit:
-                    heapq.heappush(minHeap, (max(tmp, grid[nr][nc]), nr,nc))
+                    heapq.heappush(minHeap,(max(grid[nr][nc],tmp),nr,nc))
                     visit.add((nr,nc))
-                    
-
-
-            
