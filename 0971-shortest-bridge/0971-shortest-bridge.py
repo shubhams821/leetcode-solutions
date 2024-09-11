ee81e888d8293876bc52e05b04fef72024ecaec4
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-11 08:36:18 +0530

    Time: 352 ms (19.3%), Space: 20.9 MB (5.74%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.py
+++ 0971-shortest-bridge/0971-shortest-bridge.py
@@ -1,33 +1,36 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         N = len(grid)
-        direct = [[1,0],[0,1],[0,-1],[-1,0]]
-
         visit = set()
+        directions = [[1,0],[0,1],[-1,0],[0,-1]]
+
         def dfs(r,c):
             if r not in range(N) or c not in range(N) or (r,c) in visit or not grid[r][c]:
                 return
             visit.add((r,c))
-            for dr, dc in direct:
+            for dr, dc in directions:
                 dfs(dr+r, dc+c)
         
         def bfs():
             res, q = 0, deque(visit)
             while q:
                 for i in range(len(q)):
-                    r, c = q.popleft()
-                    for dr, dc in direct:
-                        curR, curC = r+dr, c+dc
-                        if curR not in range(N) or curC not in range(N) or (curR, curC) in visit:
+                    row,col = q.popleft()
+                    for dr, dc in directions:
+                        r= row+ dr
+                        c= col+ dc
+                        if r not in range(N) or c not in range(N) or (r,c) in visit:
                             continue
-                        if grid[curR][curC]:
+                        print(grid[r][c])
+                        if grid[r][c]:
+                            print(0)
                             return res
-                        q.append([curR, curC])
-                        visit.add((curR, curC))
+                        q.append((r,c))
+                        visit.add((r,c))
                 res+=1
         for r in range(N):
             for c in range(N):
                 if grid[r][c]:
                     dfs(r,c)
+                    print(deque(visit))
                     return bfs()
-                
