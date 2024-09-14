35b25e6247627d7fbdc4ebb4b6074d4ae62a74c4
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-14 22:16:04 +0530

    Time: 380 ms (38.5%), Space: 18.6 MB (79.43%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.py
+++ 0744-network-delay-time/0744-network-delay-time.py
@@ -1,23 +1,18 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
         edges = {i:[] for i in range(1,n+1)}
-        for s, d, w in times:
+        for s,d,w in times:
             edges[s].append([d,w])
-        print(edges)
-        
         visit = set()
-        minHeap = [(0,k)]
         t = 0
+        minHeap = [(0,k)]
         while minHeap:
             w1, v1 = heapq.heappop(minHeap)
             if v1 in visit:
                 continue
             visit.add(v1)
             t = max(t, w1)
-            print(edges[v1])
             for v2, w2 in edges[v1]:
                 if v2 not in visit:
-                    heapq.heappush(minHeap,(w1+w2, v2))
-            # print(minHeap)
-
-        return t if len(visit) == n else -1
+                    heapq.heappush(minHeap, (w1+w2, v2))
+        return t if len(visit)== n else -1
