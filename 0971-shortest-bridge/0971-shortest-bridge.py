b7b5c4daa256858882a8e8065258c12c3bdda910
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-16 09:32:28 +0530

    Time: 378 ms (15.72%), Space: 21 MB (5.24%) - LeetHub

--- 0971-shortest-bridge/0971-shortest-bridge.py
+++ 0971-shortest-bridge/0971-shortest-bridge.py
@@ -1,36 +1,33 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        N = len(grid)
+        Rows, Cols = len(grid), len(grid[0])
         visit = set()
         directions = [[1,0],[0,1],[-1,0],[0,-1]]
-
-        def dfs(r,c):
-            if r not in range(N) or c not in range(N) or (r,c) in visit or not grid[r][c]:
+        def dfs(i,j):
+            if i not in range(Rows) or j not in range(Cols) or (i,j) in visit or grid[i][j] ==0:
                 return
-            visit.add((r,c))
+            visit.add((i,j))
             for dr, dc in directions:
-                dfs(dr+r, dc+c)
+                dfs(i+dr, j+dc)
         
         def bfs():
             res, q = 0, deque(visit)
+
             while q:
                 for i in range(len(q)):
-                    row,col = q.popleft()
+                    row, col = q.popleft()
+                    print(row, col)
                     for dr, dc in directions:
-                        r= row+ dr
-                        c= col+ dc
-                        if r not in range(N) or c not in range(N) or (r,c) in visit:
-                            continue
-                        print(grid[r][c])
-                        if grid[r][c]:
-                            print(0)
-                            return res
-                        q.append((r,c))
-                        visit.add((r,c))
+                        r,c = row+dr, col+dc
+                        
+                        if r in range(Rows) and c in range(Cols) and (r,c) not in visit:
+                            if grid[r][c] == 1:
+                                return res
+                            q.append((r,c))
+                            visit.add((r,c))
                 res+=1
-        for r in range(N):
-            for c in range(N):
+        for r in range(Rows):
+            for c in range(Cols):
                 if grid[r][c]:
                     dfs(r,c)
-                    print(deque(visit))
                     return bfs()
