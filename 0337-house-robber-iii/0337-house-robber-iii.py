6e25b9b8671a6d51d8d63ce9c0ef2cd4eee5cf31
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-24 23:36:02 +0530

    Time: 42 ms (79.2%), Space: 18.1 MB (86.44%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -9,6 +9,7 @@ class Solution:
         def dfs(node):
             if not node:
                 return [0,0]
+            
             left = dfs(node.left)
             right = dfs(node.right)
             withRoot = node.val + left[1] + right[1]
