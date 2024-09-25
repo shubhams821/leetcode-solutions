72330a00f364a0c2acf99394e4be19ceda4f6360
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-09-25 09:58:54 +0530

    Time: 142 ms (7.62%), Space: 25 MB (17.93%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -5,9 +5,8 @@ class Solution:
             if curr >n:
                 return
             res.append(curr)
-            # curr *= 10
             for i in range(10):
                 dfs(curr*10 +i)
-        for i in range(9):
-            dfs(i+1)
+        for i in range(1,10):
+            dfs(i)
         return res
