d2a77a3f6530a0707a0fba87e6d59daae27c854e
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-10-02 23:23:12 +0530

    Time: 126 ms (9.36%), Space: 18.9 MB (11.98%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.py
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.py
@@ -2,28 +2,16 @@ class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
-
-        def bfs(i,j):
-            q = collections.deque()
-            q.append([i,j])
-            visit.add((i,j))
-            flag = False
-            while q:
-                for i in range(len(q)):
-                    row, col = q.popleft()
-                    directions =[[1,0],[0,1],[-1,0],[0,-1]]
-                    for dr, dc in directions:
-                        r,c = row+dr, col+dc
-                        if r in range(Rows) and c in range(Cols) and (r,c) not in visit and not grid[r][c]:
-                            q.append([r,c])
-                            visit.add((r,c))
-                        if r not in range(Rows) or c not in range(Cols):
-                            flag = True
-            return 1 if not flag else 0
-        
-        island = 0
+        def dfs(r,c):
+            if r not in range(Rows) or c not in range(Cols):
+                return 0
+            if grid[r][c] or (r,c) in visit:
+                return 1
+            visit.add((r,c))
+            return min(dfs(r+1,c), dfs(r,c+1), dfs(r-1,c), dfs(r,c-1))
+        res = 0
         for r in range(Rows):
             for c in range(Cols):
                 if not grid[r][c] and (r,c) not in visit:
-                    island += bfs(r,c)
-        return island
+                    res+= dfs(r,c)
+        return res
