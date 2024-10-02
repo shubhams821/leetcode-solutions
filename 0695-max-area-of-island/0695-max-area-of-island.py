c6fad0ce46920a1f54f000bad7343d8989f1dcec
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-10-02 23:28:14 +0530

    Time: 125 ms (18.17%), Space: 18.4 MB (15.35%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,25 +2,15 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         Rows, Cols = len(grid), len(grid[0])
         visit = set()
+
+        def dfs(r,c):
+            if r not in range(Rows) or c not in range(Cols) or not grid[r][c] or (r,c) in visit:
+                return 0
+            visit.add((r,c))
+            return 1 + dfs(r+1,c) +dfs(r,c+1) + dfs(r-1,c) + dfs(r,c-1)
         res = 0
-        def bfs(i,j):
-            q = collections.deque()
-            q.append([i,j])
-            visit.add((i,j))
-            area = 1
-            while q:
-                for i in range(len(q)):
-                    row, col = q.popleft()
-                    directions = [[1,0],[0,1],[-1,0],[0,-1]]
-                    for dr, dc in directions:
-                        r,c = row+dr, col+dc
-                        if r in range(Rows) and c in range(Cols) and grid[r][c] and (r,c) not in visit:
-                            q.append([r,c])
-                            visit.add((r,c))
-                            area +=1
-            return area
         for r in range(Rows):
             for c in range(Cols):
                 if grid[r][c] and (r,c) not in visit:
-                    res = max(res, bfs(r,c))
-        return res 
+                    res = max(dfs(r,c), res)
+        return res
