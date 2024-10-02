cd22ecbac89d44faf68e2234b26b7b9918cd04f2
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-10-02 23:37:39 +0530

    Time: 114 ms (85.11%), Space: 18.2 MB (30.25%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,34 +1,30 @@
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         Rows, Cols = len(board), len(board[0])
         visit = set()
-
-        def bfs(i,j):
-            q = collections.deque()
-            q.append([i,j])
-            visit.add((i,j))
-            board[i][j] = "T" 
-            while q:
-                for i in range(len(q)):
-                    row, col = q.popleft()
-                    directions = [[1,0],[0,1],[-1,0],[0,-1]]
-                    for dr, dc in directions:
-                        r,c = row+dr, col+dc
-                        if r in range(Rows) and c in range(Cols) and board[r][c] == "O" and (r,c) not in visit:
-                            q.append([r,c])
-                            visit.add((r,c))
-                            board[r][c] = "T" 
-                            print(r,c)
+        def dfs(r,c):
+            if r not in range(Rows) or c not in range(Cols) or board[r][c] =="X" or (r,c) in visit:
+                return
+            visit.add((r,c))
+            board[r][c] = "T"
+            dfs(r,c+1)
+            dfs(r+1,c)
+            dfs(r-1,c)
+            dfs(r,c-1)
+        
         for r in range(Rows):
             for c in range(Cols):
-                if r in [0,Rows-1] or c in [0,Cols-1]:
-                    if (r,c) not in visit and board[r][c] == "O":
-                        bfs(r,c)
-                        print(r,c)
-        print(board)
+                if (r in [0,Rows-1] or c in [0,Cols-1]) and board[r][c]== "O":
+                    dfs(r,c)
+
+        
         for r in range(Rows):
             for c in range(Cols):
                 if board[r][c] == "O":
                     board[r][c] = "X"
-                elif board[r][c] == "T":
+                if board[r][c] == "T":
                     board[r][c] = "O"
+
