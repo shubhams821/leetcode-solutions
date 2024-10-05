e08b3abb90520208bb844ce5e280fd0ea0817839
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-10-05 23:54:41 +0530

    Time: 88 ms (25.01%), Space: 18.7 MB (60.29%) - LeetHub

--- 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.py
+++ 0784-insert-into-a-binary-search-tree/0784-insert-into-a-binary-search-tree.py
@@ -9,7 +9,7 @@ class Solution:
         if not root:
             return TreeNode(val)
         if val > root.val:
-            root.right =  self.insertIntoBST(root.right, val)
+            root.right = self.insertIntoBST(root.right, val)
         elif val < root.val:
             root.left = self.insertIntoBST(root.left, val)
         return root
