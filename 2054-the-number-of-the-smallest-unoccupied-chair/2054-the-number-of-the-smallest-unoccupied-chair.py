b18fb5d38ab2aa1f661c5141aa0c94bdc365c45c
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-10-11 22:45:33 +0530

    Time: 551 ms (84.99%), Space: 22.7 MB (76.28%) - LeetHub

--- 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/2054-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,15 +1,17 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        order = sorted(range(len(times)), key = lambda x: times[x][0])  # <-- 1)
-        emptySeats, takenSeats = list(range(len(times))), []            # <-- 2)
+        times = [(t[0], t[1], i)  for i,t in enumerate(times)]
+        times.sort()
 
-        for i in order:                                                 # <-- 3)
-            ar, lv = times[i]
+        used_chair = []
+        available_chair = [i for i in range(len(times))]
 
-            while takenSeats and takenSeats[0][0] <= ar:
-                heappush(emptySeats, heappop(takenSeats)[1])
-            seat = heappop(emptySeats)                                  # <-- 4)
+        for a,l, i in times:
+            while used_chair and used_chair[0][0] <= a:
+                leave, chair = heapq.heappop(used_chair)
+                heapq.heappush(available_chair, chair)
+            chair = heapq.heappop(available_chair)
+            heapq.heappush(used_chair, (l,chair))
+            if i == targetFriend:
+                return chair
 
-            if i == targetFriend: return seat
-
-            heappush(takenSeats,(lv, seat))
