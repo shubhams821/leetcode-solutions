cffa20948d2beaa8863a375641f68e8d3900c16c
shubham singh <136055036+shubhams821@users.noreply.github.com>
2024-10-13 18:48:46 +0530

    Time: 124 ms (67.63%), Space: 18.2 MB (5.02%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,9 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        l = 0
-        for i in range(len(nums)):
-            if nums[i]:
-                nums[l],nums[i] = nums[i], nums[l]
-                l+=1
-        return nums
+        i =0
+        for r in range(len(nums)):
+            if nums[r]:
+                nums[i], nums[r] = nums[r], nums[i]
+                i+=1
